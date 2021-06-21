05e7b30f7ccba4d93a5c3ad469936c622b2f8775
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-06-21 22:26:07 +0530

    slice demo

--- Main.go
+++ Main.go
@@ -4,6 +4,27 @@ import "fmt"
 
 func main() {
 
+	sliceDemo()
+}
+
+func sliceDemo() {
+
+	cards := []string{"One of Black", newCard()}
+	printSlice(cards)
+
+	cards = append(cards, "2 of Hearts")
+	printSlice(cards)
+}
+
+func printSlice(cards []string) {
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func variableDemo() {
+
 	//var card string = "Ace of Spades"
 
 	card := "Ace of Spades"
